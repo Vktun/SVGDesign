05782e38a31c6ceae2f75ec28431cb014bb2185d
vktun-office <gxzh0817@126.com>
2019-04-28 17:34:44 +0800

    lingling

--- SVGDesign/Scripts/svg-attr/svg.attr.js
+++ SVGDesign/Scripts/svg-attr/svg.attr.js
@@ -1,35 +1,38 @@
 ﻿// 组件处理模块
 (function (componentsAttr, svg) {
 
-    if (!svg) console.error("need svgJS")
+    if (!svg) console.error("need svgJS");
     var attrs = {
-        common: {
+        basic: {
 
         },
-        data: {
+        dataBasic: {
+
+        },
+        dataCustom: {
 
         }
-    }
+    };
     componentsAttr.get = function (svgEl) {
 
         if (svgEl && svgEl.attr()) {
             var a = svgEl.attr();
             console.table(a)
-            for (var i = 0,len=a.length; i < len; i++) {
-                
+            for (var i = 0, len = a.length; i < len; i++) {
+
                 //if (a[i].indexOf('data-') > -1) {
                 //    attrs.data[a[i].repalce("data-","")]=
                 //}
             }
         }
-    }
+    };
 
     function convert(input) {
         return parseInt(input);
     }
     componentsAttr.add = function (a, b) {
         return convert(a) + convert(b);
-    }
+    };
     window.componentsAttr = componentsAttr;
 })(window.componentsAttr || {}, SVG);
 
